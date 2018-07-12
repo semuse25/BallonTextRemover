e1e47defe3a251f4edccaa2895a74067b00ec520
DESKTOP-I02NIPP\JeongHyeon <semuse25@gmail.com>
2018-07-12 13:47:48 +0900

    add function

--- textMaskMakerUI.py
+++ textMaskMakerUI.py
@@ -13,58 +13,89 @@ import cv2
 import numpy as np
 import ballTextMasker
 
+
 windowH = 720
 cover = windowH//3
 mainShowArea = windowH - cover
 
 ix, iy = -1,-1
 drawing = False
+mode = 'RECT'
 img = []
 origin = []
 back = []
 mask = []
 maskBack = []
+maskTemp = []
+rad = 10
 
 def textDelete(event, x,y, flags, param):
-    global ix, iy, drawing, img, origin, back, mask, maskBack
+    global ix, iy, drawing, img, origin, back, mask, maskBack, maskTemp, mode, rad
+
+    trace = []
+
 
     if event == cv2.EVENT_LBUTTONDOWN:
-        ix = x
-        iy = y
+        if mode == 'RECT':
+            ix = x
+            iy = y
+        elif mode == 'DRAW':
+            maskTemp = np.zeros(mask.shape,np.uint8)
+            cv2.circle(img,(x,y),rad,(255,255,255),-1)
+            cv2.circle(maskTemp,(x,y),rad,(0,0,255),-1)
         drawing = True
     elif event == cv2.EVENT_MOUSEMOVE:
-        if drawing == True:
-            img = origin.copy()
-            cv2.rectangle(img,(ix,iy),(x,y),(255,0,0),1)
+        if mode == 'RECT':
+            if drawing == True:
+                img = origin.copy()
+                cv2.rectangle(img,(ix,iy),(x,y),(255,0,0),1)
+        elif mode == 'DRAW':
+            if drawing == True:
+                cv2.circle(img,(x,y),rad,(255,255,255),-1)
+                cv2.circle(maskTemp,(x,y),rad,(0,0,255),-1)
     elif event == cv2.EVENT_LBUTTONUP:
         drawing = False
-        img = origin.copy()
-        textFinder= ballTextMasker.BalloonCleaner()
-        if ix < x:
-            x1 = ix
-            x2 = x
-        else:
-            x1 = x
-            x2 = ix
-        if iy < y:
-            y1 = iy
-            y2 = y
-        else:
-            y1 = y
-            y2 = iy
-        maskBack = mask.copy()
-        maskTemp, img[y1:y2+1,x1:x2+1] = textFinder.cleanBalloon(img[y1:y2+1,x1:x2+1])
-        mask[y1:y2 + 1, x1:x2 + 1] = cv2.add(mask[y1:y2+1,x1:x2+1],maskTemp)
-        back = origin.copy()
-        origin = img.copy()
-        #cv2.rectangle(img,(ix,iy),(x,y),(255,0,0),-1)
-        #print(x-ix,y-iy)
+
+        if mode == 'RECT':
+            img = origin.copy()
+            textFinder= ballTextMasker.BalloonCleaner()
+            if ix < x:
+                x1 = ix
+                x2 = x
+            else:
+                x1 = x
+                x2 = ix
+            if iy < y:
+                y1 = iy
+                y2 = y
+            else:
+                y1 = y
+                y2 = iy
+            maskBack = mask.copy()
+            maskTemp, img[y1:y2+1,x1:x2+1] = textFinder.cleanBalloon(img[y1:y2+1,x1:x2+1])
+            mask[y1:y2 + 1, x1:x2 + 1] = cv2.add(mask[y1:y2+1,x1:x2+1],maskTemp)
+            back = origin.copy()
+            origin = img.copy()
+        elif mode == 'DRAW':
+            imgInv = cv2.bitwise_not(origin)
+            maskBack = mask.copy()
+            back = origin.copy()
+            origin = img.copy()
+            maskTemp = cv2.bitwise_and(imgInv,maskTemp)
+            mask = cv2.add(mask,maskTemp)
+
+
+
+
+
 
 def main(srcpath,dstpath) :
-    global img, origin, back, mask, maskBack
+    global img, origin, back, mask, maskBack, drawing, mode, rad
     Image=cv2.imread(srcpath,cv2.IMREAD_COLOR)
     Mask = np.zeros(Image.shape,np.uint8)
     roiNum = 1
+    reset = False
+    print('mode RECT')
     while True:
         if Image.shape[0] <= mainShowArea*(roiNum-1):
             break
@@ -79,7 +110,6 @@ def main(srcpath,dstpath) :
         back = origin.copy()
         cv2.namedWindow('image')
         cv2.setMouseCallback('image',textDelete)
-
         while True:
             cv2.imshow('image',img)
             #cv2.imshow('mask',mask)
@@ -98,7 +128,36 @@ def main(srcpath,dstpath) :
                 origin = back.copy()
                 img = origin.copy()
                 mask = maskBack.copy()
+            elif k == 113:
+                rad = 10
+                mode = 'RECT'
+                return -1
+            elif k == 114:
+                cv2.destroyAllWindows()
+                reset = True
+                rad = 10
+                mode = 'RECT'
+                break
+            elif k == 109 and drawing == False:
+                if mode == 'RECT':
+                    mode = 'DRAW'
+                    print('mode DRAW  rad = '+str(rad))
+                elif mode == 'DRAW':
+                    mode = 'RECT'
+                    print('mode RECT')
+            elif k == 43 and drawing == False and mode == 'DRAW':
+                if rad < 30:
+                    rad += 1
+                    print('mode DRAW  rad = '+str(rad))
+            elif k == 45 and drawing == False and mode == 'DRAW':
+                if rad > 1:
+                    rad -= 1
+                    print('mode DRAW  rad = '+str(rad))
 
+
+        if reset == True:
+            main(srcpath,dstpath)
+            return -1
         roiNum += 1
 
     cleanName = dstpath + "_clean.png"
@@ -106,6 +165,8 @@ def main(srcpath,dstpath) :
 
     cv2.imwrite(cleanName,Image)
     cv2.imwrite(maskName,Mask)
+
+
 
 
 if __name__ == "__main__" :
